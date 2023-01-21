6e4383b8422b80cf4f20133dfff7c277c5b1462d
Prashant Gohil <prashantgohil248@gmail.com>
2023-01-21 14:17:06 +0530

    add readDb and writeDb functions to modify file

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,11 +1,29 @@
 import { promises as fs } from 'fs';
 import { POSTS_FILE_PATH } from '../config/config.js';
 
+// Asynchronous function to read the file and parse its contents as JSON
+async function readDb() {
+  try {
+    return JSON.parse(await fs.readFile(POSTS_FILE_PATH));
+  } catch (err) {
+    console.error(err);
+    throw new Error('Error reading file');
+  }
+}
+
+// Asynchronous function to write the file with the updated data
+async function writeDb(data) {
+  try {
+    await fs.writeFile(POSTS_FILE_PATH, JSON.stringify(data, null, 2));
+  } catch (err) {
+    console.error(err);
+    throw new Error('Error writing to file');
+  }
+}
+
 export const getAllPosts = async (req, res) => {
   try {
-    const result = await fs.readFile(POSTS_FILE_PATH);
-    const data = JSON.parse(result);
-
+    const data = await readDb();
     res.json(data.posts);
   } catch (error) {
     console.log(error);
